ed6bb1417940b5cad158de412279303ea24fd436
Dilshan Perera <100390490+Thanu123456@users.noreply.github.com>
2025-04-21 07:27:49 +0400

    Updated Report Form

--- EscopeWindowsApp/Reports.Designer.cs
+++ EscopeWindowsApp/Reports.Designer.cs
@@ -28,67 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.expReportBtn = new Siticone.Desktop.UI.WinForms.SiticoneButton();
             this.saleReportBtn = new Siticone.Desktop.UI.WinForms.SiticoneButton();
             this.stockReportBtn = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.siticoneButton2 = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.topProductsPieChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.siticonePanel1 = new Siticone.Desktop.UI.WinForms.SiticonePanel();
+            this.siticoneButton1 = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.siticoneButton3 = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topProductsPieChart)).BeginInit();
             this.SuspendLayout();
             // 
             // expReportBtn
             // 
+            this.expReportBtn.Animated = true;
+            this.expReportBtn.BorderRadius = 8;
             this.expReportBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.expReportBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.expReportBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.expReportBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.expReportBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.expReportBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.expReportBtn.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.expReportBtn.ForeColor = System.Drawing.Color.White;
-            this.expReportBtn.Location = new System.Drawing.Point(29, 32);
+            this.expReportBtn.HoverState.FillColor = System.Drawing.Color.Maroon;
+            this.expReportBtn.Location = new System.Drawing.Point(358, 31);
+            this.expReportBtn.MaximumSize = new System.Drawing.Size(297, 129);
+            this.expReportBtn.MinimumSize = new System.Drawing.Size(297, 129);
             this.expReportBtn.Name = "expReportBtn";
-            this.expReportBtn.Size = new System.Drawing.Size(259, 129);
+            this.expReportBtn.Size = new System.Drawing.Size(297, 129);
             this.expReportBtn.TabIndex = 0;
-            this.expReportBtn.Text = "Expences Report";
+            this.expReportBtn.Text = "EXPENSES REPORT";
             this.expReportBtn.Click += new System.EventHandler(this.expReportBtn_Click);
             // 
             // saleReportBtn
             // 
+            this.saleReportBtn.Animated = true;
+            this.saleReportBtn.BorderRadius = 8;
             this.saleReportBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.saleReportBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.saleReportBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.saleReportBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.saleReportBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.saleReportBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.saleReportBtn.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.saleReportBtn.ForeColor = System.Drawing.Color.White;
-            this.saleReportBtn.Location = new System.Drawing.Point(333, 32);
+            this.saleReportBtn.HoverState.FillColor = System.Drawing.Color.Green;
+            this.saleReportBtn.Location = new System.Drawing.Point(29, 31);
+            this.saleReportBtn.MaximumSize = new System.Drawing.Size(297, 129);
+            this.saleReportBtn.MinimumSize = new System.Drawing.Size(297, 129);
             this.saleReportBtn.Name = "saleReportBtn";
-            this.saleReportBtn.Size = new System.Drawing.Size(259, 129);
+            this.saleReportBtn.Size = new System.Drawing.Size(297, 129);
             this.saleReportBtn.TabIndex = 1;
-            this.saleReportBtn.Text = "Sales Report";
+            this.saleReportBtn.Text = "SALES REPORT";
             this.saleReportBtn.Click += new System.EventHandler(this.saleReportBtn_Click);
             // 
             // stockReportBtn
             // 
+            this.stockReportBtn.Animated = true;
+            this.stockReportBtn.BorderRadius = 8;
             this.stockReportBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.stockReportBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.stockReportBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
             this.stockReportBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.stockReportBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.stockReportBtn.FillColor = System.Drawing.Color.Indigo;
+            this.stockReportBtn.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.stockReportBtn.ForeColor = System.Drawing.Color.White;
-            this.stockReportBtn.Location = new System.Drawing.Point(333, 208);
+            this.stockReportBtn.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.stockReportBtn.Location = new System.Drawing.Point(29, 182);
+            this.stockReportBtn.MaximumSize = new System.Drawing.Size(297, 129);
+            this.stockReportBtn.MinimumSize = new System.Drawing.Size(297, 129);
             this.stockReportBtn.Name = "stockReportBtn";
-            this.stockReportBtn.Size = new System.Drawing.Size(259, 129);
+            this.stockReportBtn.Size = new System.Drawing.Size(297, 129);
             this.stockReportBtn.TabIndex = 2;
-            this.stockReportBtn.Text = "Stock Report";
+            this.stockReportBtn.Text = "STOCK REPORT";
             this.stockReportBtn.Click += new System.EventHandler(this.stockReportBtn_Click);
+            // 
+            // siticoneButton2
+            // 
+            this.siticoneButton2.Animated = true;
+            this.siticoneButton2.BorderRadius = 8;
+            this.siticoneButton2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.siticoneButton2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.siticoneButton2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneButton2.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton2.HoverState.FillColor = System.Drawing.Color.Navy;
+            this.siticoneButton2.Location = new System.Drawing.Point(358, 182);
+            this.siticoneButton2.MaximumSize = new System.Drawing.Size(297, 129);
+            this.siticoneButton2.MinimumSize = new System.Drawing.Size(297, 129);
+            this.siticoneButton2.Name = "siticoneButton2";
+            this.siticoneButton2.Size = new System.Drawing.Size(297, 129);
+            this.siticoneButton2.TabIndex = 4;
+            this.siticoneButton2.Text = "STOCK REPORT";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.chart1.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.Scaled;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.BorderColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(693, 31);
+            this.chart1.Margin = new System.Windows.Forms.Padding(0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(671, 316);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // topProductsPieChart
+            // 
+            this.topProductsPieChart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.topProductsPieChart.BackColor = System.Drawing.Color.Transparent;
+            this.topProductsPieChart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.topProductsPieChart.BorderlineColor = System.Drawing.Color.Transparent;
+            chartArea2.Name = "ChartArea1";
+            this.topProductsPieChart.ChartAreas.Add(chartArea2);
+            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend2.Name = "Legend1";
+            this.topProductsPieChart.Legends.Add(legend2);
+            this.topProductsPieChart.Location = new System.Drawing.Point(706, 347);
+            this.topProductsPieChart.Margin = new System.Windows.Forms.Padding(0);
+            this.topProductsPieChart.Name = "topProductsPieChart";
+            this.topProductsPieChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            series2.IsValueShownAsLabel = true;
+            series2.LabelForeColor = System.Drawing.Color.White;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.topProductsPieChart.Series.Add(series2);
+            this.topProductsPieChart.Size = new System.Drawing.Size(671, 399);
+            this.topProductsPieChart.TabIndex = 12;
+            this.topProductsPieChart.Text = "chart2";
+            title1.Alignment = System.Drawing.ContentAlignment.TopLeft;
+            title1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.Text = "Top 5 Produts";
+            this.topProductsPieChart.Titles.Add(title1);
+            this.topProductsPieChart.Click += new System.EventHandler(this.topProductsPieChart_Click);
+            // 
+            // siticonePanel1
+            // 
+            this.siticonePanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.siticonePanel1.FillColor = System.Drawing.Color.Transparent;
+            this.siticonePanel1.Location = new System.Drawing.Point(0, 751);
+            this.siticonePanel1.Name = "siticonePanel1";
+            this.siticonePanel1.Size = new System.Drawing.Size(1386, 37);
+            this.siticonePanel1.TabIndex = 15;
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.Animated = true;
+            this.siticoneButton1.BorderRadius = 8;
+            this.siticoneButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.siticoneButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoverState.FillColor = System.Drawing.Color.Navy;
+            this.siticoneButton1.Location = new System.Drawing.Point(358, 337);
+            this.siticoneButton1.MaximumSize = new System.Drawing.Size(297, 129);
+            this.siticoneButton1.MinimumSize = new System.Drawing.Size(297, 129);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.Size = new System.Drawing.Size(297, 129);
+            this.siticoneButton1.TabIndex = 17;
+            this.siticoneButton1.Text = "STOCK REPORT";
+            // 
+            // siticoneButton3
+            // 
+            this.siticoneButton3.Animated = true;
+            this.siticoneButton3.BorderRadius = 8;
+            this.siticoneButton3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.siticoneButton3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.siticoneButton3.FillColor = System.Drawing.Color.Indigo;
+            this.siticoneButton3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneButton3.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton3.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.siticoneButton3.Location = new System.Drawing.Point(29, 337);
+            this.siticoneButton3.MaximumSize = new System.Drawing.Size(297, 129);
+            this.siticoneButton3.MinimumSize = new System.Drawing.Size(297, 129);
+            this.siticoneButton3.Name = "siticoneButton3";
+            this.siticoneButton3.Size = new System.Drawing.Size(297, 129);
+            this.siticoneButton3.TabIndex = 16;
+            this.siticoneButton3.Text = "STOCK REPORT";
             // 
             // Reports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1386, 788);
+            this.ControlBox = false;
+            this.Controls.Add(this.siticoneButton1);
+            this.Controls.Add(this.siticoneButton3);
+            this.Controls.Add(this.siticonePanel1);
+            this.Controls.Add(this.topProductsPieChart);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.siticoneButton2);
             this.Controls.Add(this.stockReportBtn);
             this.Controls.Add(this.saleReportBtn);
             this.Controls.Add(this.expReportBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimizeBox = false;
             this.Name = "Reports";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reports";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topProductsPieChart)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -98,5 +273,11 @@
         private Siticone.Desktop.UI.WinForms.SiticoneButton expReportBtn;
         private Siticone.Desktop.UI.WinForms.SiticoneButton saleReportBtn;
         private Siticone.Desktop.UI.WinForms.SiticoneButton stockReportBtn;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton siticoneButton2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart topProductsPieChart;
+        private Siticone.Desktop.UI.WinForms.SiticonePanel siticonePanel1;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton siticoneButton3;
     }
 }
