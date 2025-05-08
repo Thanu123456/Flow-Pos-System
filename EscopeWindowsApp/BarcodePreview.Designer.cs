8017986d8c640c86b3f1150ade802eb8cdfab672
Dilshan Perera <100390490+Thanu123456@users.noreply.github.com>
2025-05-08 12:03:50 +0400

    Updated Barcode Print

---
+++ EscopeWindowsApp/BarcodePreview.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace EscopeWindowsApp
+{
+    partial class BarcodePreview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.barcodePreBorderlessForm = new Siticone.Desktop.UI.WinForms.SiticoneBorderlessForm(this.components);
+            this.headerPanel = new Siticone.Desktop.UI.WinForms.SiticonePanel();
+            this.controlPanel = new Siticone.Desktop.UI.WinForms.SiticonePanel();
+            this.closeBtn = new Siticone.Desktop.UI.WinForms.SiticoneControlBox();
+            this.barcodePreviewPicBox = new Siticone.Desktop.UI.WinForms.SiticonePictureBox();
+            this.headerPanel.SuspendLayout();
+            this.controlPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barcodePreviewPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barcodePreBorderlessForm
+            // 
+            this.barcodePreBorderlessForm.BorderRadius = 25;
+            this.barcodePreBorderlessForm.ContainerControl = this;
+            this.barcodePreBorderlessForm.DockIndicatorTransparencyValue = 0.6D;
+            this.barcodePreBorderlessForm.TransparentWhileDrag = true;
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.White;
+            this.headerPanel.Controls.Add(this.controlPanel);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.FillColor = System.Drawing.Color.White;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(610, 34);
+            this.headerPanel.TabIndex = 29;
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.controlPanel.BackColor = System.Drawing.Color.White;
+            this.controlPanel.Controls.Add(this.closeBtn);
+            this.controlPanel.FillColor = System.Drawing.Color.White;
+            this.controlPanel.Location = new System.Drawing.Point(518, 2);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(92, 30);
+            this.controlPanel.TabIndex = 1;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeBtn.BorderRadius = 8;
+            this.closeBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.closeBtn.IconColor = System.Drawing.Color.White;
+            this.closeBtn.Location = new System.Drawing.Point(45, 2);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(26, 26);
+            this.closeBtn.TabIndex = 0;
+            // 
+            // barcodePreviewPicBox
+            // 
+            this.barcodePreviewPicBox.ImageRotate = 0F;
+            this.barcodePreviewPicBox.Location = new System.Drawing.Point(12, 52);
+            this.barcodePreviewPicBox.Name = "barcodePreviewPicBox";
+            this.barcodePreviewPicBox.Size = new System.Drawing.Size(577, 596);
+            this.barcodePreviewPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.barcodePreviewPicBox.TabIndex = 30;
+            this.barcodePreviewPicBox.TabStop = false;
+            this.barcodePreviewPicBox.Click += new System.EventHandler(this.barcodePreviewPicBox_Click);
+            // 
+            // BarcodePreview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 660);
+            this.Controls.Add(this.barcodePreviewPicBox);
+            this.Controls.Add(this.headerPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "BarcodePreview";
+            this.Text = "BarcodePreview";
+            this.Load += new System.EventHandler(this.BarcodePreview_Load);
+            this.headerPanel.ResumeLayout(false);
+            this.controlPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.barcodePreviewPicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticoneBorderlessForm barcodePreBorderlessForm;
+        private Siticone.Desktop.UI.WinForms.SiticonePictureBox barcodePreviewPicBox;
+        private Siticone.Desktop.UI.WinForms.SiticonePanel headerPanel;
+        private Siticone.Desktop.UI.WinForms.SiticonePanel controlPanel;
+        private Siticone.Desktop.UI.WinForms.SiticoneControlBox closeBtn;
+    }
+}
